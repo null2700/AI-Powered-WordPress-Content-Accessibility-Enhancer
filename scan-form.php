8b19977006dc5b5e07ae7bc80c6e1523b0de03de
null2700 <guravsoham182@gmail.com>
2025-06-05 20:24:43 +0530

    Create scan-form.php

---
+++ scan-form.php
@@ -0,0 +1,5 @@
+<div class="accessibility-container">
+    <textarea id="htmlInput" class="styled-textarea" rows="10" placeholder="Paste page/post HTML here..."></textarea>
+    <button id="scanBtn" class="styled-button">Scan for Accessibility</button>
+    <pre id="accessibilityOutput" class="styled-output"></pre>
+</div>
